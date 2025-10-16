c6ca8761c334aca9b0701c40ab321bc971692b1c
Sergey <ssrazgonov@gmail.com>
2025-10-16 14:55:59 +0300

    feat(FileQueue): add and bootsrap queue component for commands

--- app/config/console.php
+++ app/config/console.php
@@ -6,7 +6,7 @@ $db = require __DIR__ . '/db.php';
 $config = [
     'id' => 'basic-console',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
+    'bootstrap' => ['log', 'queue'],
     'controllerNamespace' => 'app\commands',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
@@ -26,6 +26,9 @@ $config = [
             ],
         ],
         'db' => $db,
+        'queue' => [
+            'class' => \yii\queue\file\Queue::class,
+        ],
     ],
     'params' => $params,
     /*
